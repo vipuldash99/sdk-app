199408b9afa116df2a37b43e755bd9924bce0245
ashish.dash <ashish.dash@oracle.com>
2023-03-28 11:23:28 +0530

    RoomServiceClient

--- RoomServiceClient.ts
+++ RoomServiceClient.ts
@@ -1,15 +1,15 @@
-import { RoomServiceClient, Room } from 'livekit-server-sdk';
-const livekitHost = 'http://localhost:7880';
+import { RoomServiceClient, Room, ParticipantInfo } from 'livekit-server-sdk';
+const livekitHost = 'http://localhost:7880';//livekit server host
 const svc = new RoomServiceClient(livekitHost, 'devkey', 'secret');
-
+var participant_meta
 // list rooms
 svc.listRooms().then((rooms: Room[]) => {
   console.log('existing rooms', rooms);
 });
 
-// create a new room
+//create a new room
 const opts = {
-  name: 'myroom',
+  name: 'test-room',
   // timeout in seconds
   emptyTimeout: 10 * 60,
   maxParticipants: 20,
@@ -18,7 +18,34 @@ svc.createRoom(opts).then((room: Room) => {
   console.log('room created', room);
 });
 
-// delete a room
-svc.deleteRoom('myroom').then(() => {
-  console.log('room deleted');
+
+//ListAllParticipants
+svc.listParticipants('test-room').then((participants: ParticipantInfo[] ) => {
+  console.log('participants list',participants);
 });
+
+//GetParticiapantMetadata
+svc.getParticipant('test-room','user1').then((participant: ParticipantInfo) => {
+  console.log('participant meta',participant);
+  participant_meta=participant
+});
+
+//Remove Participant
+svc.removeParticipant('test-room','user1').then(() => {
+  console.log('participant removed');
+});
+
+//delete a room
+svc.deleteRoom('test-room').then(() => {
+  console.log('room deleted');
+
+});
+
+// svc.updateParticipant('test-room','user1')
+
+// svc.mutePublishedTrack('test-room','user1','PA_NdD3Fb9bM5jq',true).then(() => {
+//   console.log('participant muted');
+// });
+
+
+
